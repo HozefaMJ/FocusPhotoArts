b660a77861cf29a3ee27dabac2f6533e7b173cdf
HozefaMJ <hozefa24imp@gmail.com>
2020-09-21 20:19:29 +0530

    recheck

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect
+import requests
 import smtplib
 
 app = Flask(__name__, template_folder='./templates',
@@ -21,15 +22,17 @@ def form():
         ". His/Her email is " + email + \
         ". His/Her message is " + message
 
-    #print(query)
+    requests.post(
+        "https://api.mailgun.net/v3/sandboxc0477641953d478e97d3438bba118e0b.mailgun.org/messages",
+        auth=("api", "9932e02af2348fa99b2af60a5a3f900b-d5e69b0b-70ba3b86"),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxc0477641953d478e97d3438bba118e0b.mailgun.org>",
+              "to": "HOZEFA JAORAWALA <hozefa.jaorawala@somaiya.edu>",
+              "subject": "New Website Query",
+              "text": "Hi FocusPhotoArts you have a new query from " + name +
+              ". His/Her email is " + email +
+              ". His/Her message is " + message})
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login("hozefa.queryinfo@gmail.com", "HozefaJ@96")
-    server.sendmail("hozefa.queryinfo@gmail.com",
-                    "hozefa24imp@gmail.com", query)
-
-    return("success")
+    return "success"
 
 
 if __name__ == '__main__':
